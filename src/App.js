3d257e9fadf19a393ff9261065ff51f86605e477
DevinP6lluste <devinpolluste@gmail.com>
2020-11-25 18:09:28 +0200

    created login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Login from './Login';
 
 function App() {
   return (
     // BEM
     <div className="app">
-      <h1>Spotify</h1>
+      <Login />
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,6 @@
+// https://developer.spotify.com/
+// documentation/web-playback-sdk/quick-start/#
+
+export const authEndPoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
