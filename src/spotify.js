76f3608dcc98f5a5015e44b741c4fb4bcce88833
Devin Põlluste <devinpolluste@gmail.com>
2020-11-28 15:47:35 +0200

    Login button redirect

--- src/spotify.js
+++ src/spotify.js
@@ -4,3 +4,15 @@
 export const authEndPoint = "https://accounts.spotify.com/authorize";
 
 const redirectUri = "http://localhost:3000/";
+
+const clientId = "3bea25e5400842c19833f875050baf2c";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
