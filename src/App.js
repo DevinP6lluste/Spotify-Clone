f734a7d435e671e559fdc2bc7dc64811d6ad4a0b
Devin Põlluste <devinpolluste@gmail.com>
2020-11-29 21:21:28 +0200

    URL Token

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from './Login';
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+
+  // Run code based on given condition
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log('I have a token', token);
+  }, []);
+
   return (
     // BEM
     <div className="app">
