3cf8c74454cf620840e827c596a5bd5de4c7c09d
Devin Põlluste <devinpolluste@gmail.com>
2020-12-06 12:13:23 +0200

    Colors

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,10 @@ import "./Sidebar.css";
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+            <img className="sidebar__logo"
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
+                alt="" 
+            />
         </div>
     );
 }
