b315e736d451dead6ec0c4f0ebf6ec4c8b46c794
Devin Põlluste <devinpolluste@gmail.com>
2020-12-09 21:14:08 +0200

    Footer

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    //token: 
-    //'BQCohZbtQDJYo4Helt4yThjBP-IhbbAF-FfTuLUiY1Sm5AEBbILPBM5LHlSMfs_AT-q6L7LIGAP7U0rvmgMpB68QLrS7bdlRpf8CiaP1F1Qp5jauDXWLB2rNXL8z4C7_COz8IS-ZOEZHSFTL7ikoRuCooeX9sNQa'
+    token: 
+        'BQCohZbtQDJYo4Helt4yThjBP-IhbbAF-FfTuLUiY1Sm5AEBbILPBM5LHlSMfs_AT-q6L7LIGAP7U0rvmgMpB68QLrS7bdlRpf8CiaP1F1Qp5jauDXWLB2rNXL8z4C7_COz8IS-ZOEZHSFTL7ikoRuCooeX9sNQa'
 };
 
 const reducer = (state, action) => {
@@ -13,19 +13,19 @@ const reducer = (state, action) => {
 // Action -> type, [payload]
 
     switch(action.type) {
-        case 'SET_USER':
+        case "SET_USER":
             return {
                 ...state,
                 user: action.user,
             };
         
-        case 'SET_TOKEN':
+        case "SET_TOKEN":
             return {
                 ...state,
                 token: action.token,
             };
 
-        case 'SET_PLAYLISTS':
+        case "SET_PLAYLISTS":
             return {
                 ...state,
                 token: action.playlists,
