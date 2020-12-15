9800c17713a064ebe69112d06d3ab3f38f69e478
Devin Põlluste <devinpolluste@gmail.com>
2020-12-15 13:20:27 +0200

    Header styling

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar } from "@material-ui/core";
 
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
-
+                <SearchIcon />
+                <input
+                    placeholder="Search for Artists, Songs, or Podcasts"
+                    type="text" 
+                />
             </div>
 
             <div className="header__right">
-
+                <Avatar src="" alt="D" />
+                <h4>Devin</h4>
             </div>
-            
         </div>
     );
 }
