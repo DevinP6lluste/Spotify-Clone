1baaac559f354aca458eda8954690cd112a1119d
Devin Põlluste <devinpolluste@gmail.com>
2020-12-28 13:11:06 +0200

    Song Row

--- src/Body.js
+++ src/Body.js
@@ -5,6 +5,7 @@ import { useDataLayerValue } from "./DataLayer";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import SongRow from "./SongRow";
 
 function Body({ spotify }) {
     const [{ discover_weekly }, dispatch] = useDataLayerValue();
@@ -31,7 +32,10 @@ function Body({ spotify }) {
                 <FavoriteIcon fontSize="large" />
                 <MoreHorizIcon />
             </div>
-
+            {/* List of songs */}
+            {discover_weekly?.tracks.items.map((item) => (
+                <SongRow track={item.track} />
+            ))}
             </div>
         </div>
     );
